754fad133798c512c88a839bb8c29b900a44bdf8
Elvin-njeru <elvinnjeru@gmail.com>
2024-11-17 23:27:39 +0300

    Logic Error
    it was calculating the total price of all customers as one instead of individually

--- Question 3(Credit risk analyzer)/main.c
+++ Question 3(Credit risk analyzer)/main.c
@@ -1,70 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int price, quantity,customers, counter = 0;
-    float credit_limit, value, balance;
-    float totalPrice = 0.0;
+int main()
+{
+    int counter1,customers,quantity,counter2;
+    float credit_limit,price,total_price;
+    counter1=0;
+    printf("Enter the number of customers:");
+    scanf("%d",&customers);
 
+    while (counter1<customers)
+    {
+        counter1=counter1+1;
+        total_price=0;
+        printf("Customer:%d \n",counter1);
+        printf("Enter your credit limit:");
+        scanf("%f",&credit_limit);
+        printf("Enter your quantity of items:");
+        scanf("%d",&quantity);
+        counter2=0;
 
-    printf("Please enter the number of customers you would like to serve:\n ");
-    scanf("%d", &customers);
-
-
-    while (counter < customers) {
-        printf("Customer %d:\n", counter+ 1);
-
-
-        printf("Enter  your credit limit: ");
-        scanf("%f", &credit_limit);
-         printf("Enter quantity of goods you wish to purchase: ");
-        scanf("%d", &quantity);
-
-
-    float prices[quantity];
-
-    for (int i = 0; i < quantity; i++) {
-        printf("Enter the price of item %d: ", i + 1);
-        scanf("%f", &prices[i]);
-        totalPrice += prices[i];
-    }
-
-
-    printf("\nPrices of the items:\n");
-    for (int i = 0; i < quantity; i++) {
-        printf("Price of item %d: %.2f\n", i + 1, prices[i]);
-    }
-
-    printf("\nTotal Price: %.2f\n", totalPrice);
-
-
-
-
-
-
-        balance = credit_limit - totalPrice;
-        value = totalPrice;
-
-        while (value > credit_limit) {
-            printf("Sorry you cannot purchase goods worthy such a value on credit.\n");
-              printf("Please enter another quantity of goods you would like to purchase:\n ");
-            scanf("%d", &quantity);
-            printf("Enter the  price:\n ");
-            scanf("%d", &price);
-
-
-            value = price * quantity;
+        while(counter2<quantity){
+            counter2=counter2+1;
+            printf("Enter the price of an item:");
+            scanf("%f",&price);
+            total_price=total_price+price;
 
         }
 
 
-        printf("Your total purchase is Ksh. %.2f\n", value);
-        printf("Your balance is Ksh: %2f\n", balance);
 
 
-        counter++;
+        if (total_price>credit_limit)
+        {
+            printf("Sorry you cannot purchase goods worthy such a value on credit\n");
+            while (credit_limit<total_price)
+            {
+                printf("Enter the another quantity of item:");
+                scanf("%d",&quantity);
+                total_price=0;
+                counter2=0;
+                while(counter2<quantity){
+                    counter2=counter2+1;
+                    printf("Enter the price of an item:");
+                    scanf("%f",&price);
+                    total_price=total_price+price;
+
+                }
+                printf("\nYour total cost is %.2f \n",total_price);
+                printf("Thank You for purchasing from us\n");
+            }
+        }else
+        {
+            printf("Your total cost is %.2f \n",total_price);
+            printf("Thank You for purchasing from us\n");
+        }
+
+
     }
+
+
     return 0;
 }
-
-
-
